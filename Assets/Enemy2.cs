f6bdbea81d41dcaf3431dd97e1b70cdf9181fb16
Dharmraj1216 <dharmraj20293841@gmail.com>
2026-05-29 20:58:38 +0530

    Health System and Enemy Feedback
    Health System and Enemy feedback updated.

--- Assets/Enemy2.cs
+++ Assets/Enemy2.cs
@@ -1,17 +1,58 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
     Rigidbody rb;
+
+    private Renderer enemyRenderer;
+
+    private Color originalColor;
+
+    [Header("Enemy Health")]
+    [SerializeField] private int health = 3;
+
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Start()
+    {
+        enemyRenderer = GetComponent<Renderer>();
+
+        originalColor = enemyRenderer.material.color;
+    }
+
     public void TakeHit(Vector3 hitDirection, float force)
     {
+        // Health AI system
+        health--;
+        Debug.Log("Enemy Health " + health);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // To fresh store the hit direction
         rb.linearVelocity = Vector3.zero;
 
         rb.AddForce(hitDirection * force, ForceMode.Impulse);
+
+        // Use to execute timed / paused methods 
+        StartCoroutine(HitFlash());
+    }
+
+    // Timed / Paused Method 
+    private IEnumerator HitFlash()
+    {
+        enemyRenderer.material.color = Color.red;
+
+        yield return new WaitForSeconds(0.25f);
+
+        enemyRenderer.material.color = originalColor;
     }
 }
